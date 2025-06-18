12fcf932880a4a17d5362b75b7110ff5e8f48238
Ubuntu <ubuntu@ip-172-31-92-12.ec2.internal>
2025-06-18 17:20:16 +0000

    practice - 1

---
+++ Hello.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# objective 
+# Define a variable with the string 'Hello World'
+#Print the value of the variable you've defined and redirect the output to the file "amazing_output.txt"
+
+
+STRING='Hello-world'
+echo $STRING > amazing_output.txt 
+
+
+
+
+
+
+
